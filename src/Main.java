54a707b086a78fc3677fbc42a5ebf372fb0c52e9
dofpo <z-salawat91@mail.ru>
2022-04-26 17:40:29 +0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 100;//начальная сумма на счету клиента
-        int b = 100;//сумма пополнения счета
-        if (b >= 1000) {
-            int c = (b / 100) + b;
-            System.out.println("Количество начисленных баллов =" + " " + (c - b) + " " + "рублей");
-            System.out.println("Итоговая сумма=" + " " + (c + a) + " " + "рублей");
+        int balance = 100;//начальная сумма на счету клиента
+        int refill = 100;//сумма пополнения счета
+        if (refill >= 1000) {
+            int bonus = (refill / 100) + refill;
+            System.out.println("Количество начисленных баллов =" + " " + (bonus - refill) + " " + "рублей");
+            System.out.println("Итоговая сумма=" + " " + (bonus + balance) + " " + "рублей");
         } else {
             System.out.println("Количество начисленных баллов =" + " " + 0 + " " + "рублей");
-            System.out.println("Итоговая сумма=" + " " + (a + b) + " " + "рублей");
+            System.out.println("Итоговая сумма=" + " " + (balance + refill) + " " + "рублей");
         }
     }
 }
